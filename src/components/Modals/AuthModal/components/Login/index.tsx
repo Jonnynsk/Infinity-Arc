07ca2a413d758222df9300cc43ad59dc2bfe2f8c
Jonnynsk <@email.com>
2025-11-10 22:09:26 +0700

    feat: input

--- src/components/Modals/AuthModal/components/Login/index.tsx
+++ src/components/Modals/AuthModal/components/Login/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 
 import styles from "./styles/index.module.scss";
@@ -7,12 +10,32 @@ interface IProps {
 }
 
 export const Login = ({ onClose = () => {} }: IProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={styles.login}>
       <h2 className={styles.login__title}>Welcome Back</h2>
       <p className={styles.login__description}>
         Continue your journey to greatness
       </p>
+
+      <div className={styles.login__inputs}>
+        <Input
+          label="Email Address"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={setEmail}
+        />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={setPassword}
+        />
+      </div>
 
       <Button
         title="Login"
